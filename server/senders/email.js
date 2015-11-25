cc61b401d08b1861a5721b4280e080c084412c9d
Doug Shamoo <doug.shamoo@gmail.com>
2015-11-25 00:01:53 -0800

    (deploy) Add env variables for local and deployed

--- server/senders/email.js
+++ server/senders/email.js
@@ -1,13 +1,24 @@
 var nodemailer = require('nodemailer');
-var config = require('../config');
+
+var gmailUser;
+var gmailPass;
+
+if (process.env.GMAIL_USER) {
+  gmailUser = process.env.GMAIL_USER;
+  gmailPass = process.env.GMAIL_PASS;
+} else {
+  var config = require('../config');
+  gmailUser = config.gmail.user;
+  gmailPass = config.gmail.pass;
+}
 
 module.exports = function(email, message, cb) {
   // create reusable transporter object using SMTP transport
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: config.gmail.user,
-      pass: config.gmail.pass,
+      user: gmailUser,
+      pass: gmailPass,
     },
   });
 
