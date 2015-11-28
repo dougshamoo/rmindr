25071adf5421fec7ad8f5be7c13086ba49b2daed
Doug Shamoo <doug.shamoo@gmail.com>
2015-11-28 00:04:35 -0800

    (feat) Implement basic clock process and kue.js job queue [Closes #3]
    - On post to /email, `email` job is added to the queue, when clock.js
    is run, it currently process all jobs of type email and removes them
    from the queue
    - Only works locally until a redis addon is set up for heroku

---
+++ clock.js
@@ -0,0 +1,50 @@
+var kue = require('kue');
+var url = require('url');
+var redis = require('kue/lib/redis');
+
+var sendEmail = require('./senders/email');
+
+// make sure we use the Heroku Redis To Go URL
+// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+
+if (process.env.REDISTOGO_URL) {
+  kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(redisUrl.port, redisUrl.hostname);
+    if (redisUrl.auth) {
+      client.auth(redisUrl.auth.split(':')[1]);
+    }
+
+    return client;
+  };
+}
+
+var queue = kue.createQueue();
+
+queue.on('job enqueue', function(id, type) {
+  console.log('Job %s got queued of type %s', id, type);
+}).on('job complete', function(id, result) {
+  kue.Job.get(id, function(err, job) {
+    if (err) return;
+    job.remove(function(err) {
+      if (err) throw err;
+      console.log('removed completed job #%d', job.id);
+    });
+  });
+});
+
+// TODO: process only when given datetime is reached
+queue.process('email', function(job, done) {
+  console.log('Processing job #', job.id, ':\n', job.data);
+  sendEmail(job.data.to, job.data.message, done);
+});
+
+// function email(address, message, done) {
+//   // TODO: address checking maybe
+//   if (2 > 3) {
+//     return done(new Error('some error'));
+//   }
+
+//   // TODO: email send stuff...
+//   done();
+// }
--- server/routes.js
+++ server/routes.js
@@ -1,23 +1,73 @@
 var app = require('./server');
 var sendEmail = require('../senders/email');
 
+var kue = require('kue');
+var url = require('url');
+var redis = require('kue/lib/redis');
+
+// make sure we use the Heroku Redis To Go URL
+// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+
+if (process.env.REDISTOGO_URL) {
+  kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(redisUrl.port, redisUrl.hostname);
+    if (redisUrl.auth) {
+      client.auth(redisUrl.auth.split(':')[1]);
+    }
+
+    return client;
+  };
+}
+
+var queue = kue.createQueue();
+
+queue.on('job enqueue', function(id, type) {
+  console.log('Job %s got queued of type %s', id, type);
+}).on('job complete', function(id, result) {
+  kue.Job.get(id, function(err, job) {
+    if (err) return;
+    job.remove(function(err) {
+      if (err) throw err;
+      console.log('removed completed job #%d', job.id);
+    });
+  });
+});
+
 app.post('/email', function(req, res) {
-  console.log('REQ.BODY:', req.body);
+  // console.log('REQ.BODY:', req.body);
   var email = req.body.email;
   var message = req.body.message;
+
+  // TODO: handle timezone differences, timezone currently not sent from client
   var datetime = req.body.datetime;
   var date = new Date(datetime);
-  console.log(datetime);
-  console.log(date);
-  res.status(200).end('Awesome. Check your email!');
 
-  // sendEmail(email, message, function(err, info) {
-  //   if (err) {
-  //     console.log('An error occured: ' + err);
-  //     res.status(404).end('Whoops... something went wrong.');
-  //   }
+  // console.log(datetime);
+  // console.log(date);
+  var job = queue.create('email', {
+    to: email,
+    from: 'rmindrdev@gmail.com',
+    message: message,
+    datetime: date,
+  }).save(function(err) {
+    if (err) console.log(err);
+  });
 
-  //   console.log('Message sent: ' + info.response);
-  //   res.status(200).end('Awesome. Check your email!');
+  // TODO: Figure out why these don't work, queue level events here and in clock.js do
+
+  // job.on('complete', function(result) {
+  //   console.log('Job completed with data ', result);
+
+  // }).on('failed attempt', function(errorMessage, doneAttempts) {
+  //   console.log('Job failed');
+
+  // }).on('failed', function(errorMessage) {
+  //   console.log('Job failed');
+
+  // }).on('progress', function(progress, data) {
+  //   console.log('\r  job #' + job.id + ' ' + progress + '% complete with data ', data);
   // });
+
+  res.status(200).end('Awesome. Check your email!');
 });
