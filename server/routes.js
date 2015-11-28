0e17872cd2e5e51af90ca3d160a032072d40c618
Doug Shamoo <doug.shamoo@gmail.com>
2015-11-28 00:46:59 -0800

    (bug)(deploy) Fix redis to go addon connection for heroku [Closes #6]

--- server/routes.js
+++ server/routes.js
@@ -5,28 +5,20 @@ var kue = require('kue');
 var url = require('url');
 var redis = require('kue/lib/redis');
 
-// make sure we use the Heroku Redis To Go URL
-// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+var kueOptions = {};
 
-// if (process.env.REDISTOGO_URL) {
-//   kue.redis.createClient = function() {
-//     var redisUrl = url.parse(process.env.REDISTOGO_URL);
-//     var client = redis.createClient(redisUrl.port, redisUrl.hostname);
-//     if (redisUrl.auth) {
-//       client.auth(redisUrl.auth.split(':')[1]);
-//     }
-
-//     return client;
-//   };
-// }
-var queue;
 if (process.env.REDISTOGO_URL) {
-  queue = kue.createQueue({
-    redis: 'process.env.REDISTOGO_URL',
-  });
-} else {
-  queue = kue.createQueue();
+  var redisUrl = url.parse(process.env.REDISTOGO_URL);
+  kueOptions.redis = {
+    port: parseInt(redisUrl.port),
+    host: redisUrl.hostname,
+  };
+  if (redisUrl.auth) {
+    kueOptions.redis.auth = redisUrl.auth.split(':')[1];
+  }
 }
+
+var queue = kue.createQueue(kueOptions);
 
 queue.on('job enqueue', function(id, type) {
   console.log('Job %s got queued of type %s', id, type);
